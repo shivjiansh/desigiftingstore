f0a595036ec2853df253d1ccb7f22d58ad940945
Shivansh Jauhari <shivansh.jauhari@gmail.com>
2025-08-30 05:18:01 +0530

    console.log

--- next.config.js
+++ next.config.js
@@ -28,7 +28,8 @@ const nextConfig = {
     ];
   },
   compiler: {
-    removeConsole: process.env.NODE_ENV === "production",
+  
+    removeConsole: false,
   },
   experimental: {
     optimizeCss: true,
